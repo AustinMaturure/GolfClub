59058e3a520ef421f9ba325bdbe37f55b010c467
Austin <austinematurure@gmail.com>
2024-07-17 01:15:39 +0300

    Added: Added Pricing Section

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -37,13 +37,15 @@ function App() {
   };
 
   const faqs = [
-    { question: "What's The Golf Season?", answer: "All Round. Our Golf Season ranges from winter to summer, anything is Golf Season" },
+    { question: "What's The Golf Season?", answer: "All Round. Our Golf Season ranges from winter to summer, anytime is Golf Season" },
     { question: "Are Caddies Available?", answer: "Yes. Caddies are available to Rent For ZAR20" },
     { question: "Is There A Pitching/Chipping area?", answer: "Yes. Both a Pitching and Chipping area are available" },
     { question: "What is the Dress Code?", answer: "Appropriate Golf Attire. Anything you'd wear at the Open" },
     { question: "Is it Beginner Friendly?", answer: "Yes. Practice and Instructors are available." },
     { question: "Is Food Available on the Premises?", answer: "Yes. A Bar and a Halfway House are available" },
-    { question: "Are Credit Cards Accepted?", answer: "Yes. We accept Visa & MasterCard" }
+    { question: "Are Credit Cards Accepted?", answer: "Yes. We accept Visa & MasterCard" },
+    { question: "Are Memberships Available?", answer: "Yes. We offer competitive annual membership fees." },
+    { question: "What is The Price For a Golf Game?", answer: "ZAR40 For Affliated members. And ZAR70 for Visitors" }
   ];
 
 
@@ -271,6 +273,71 @@ The club is situated on the N2 in the south-east corner of <span className='ital
         ))}
       </div>
     </section>
+    <section className='pricing'>
+     
+      <div className="plans">
+      
+      <div className='prices-cnt'>
+        <h1><span className='italic'>Premium</span>
+        </h1>
+        <p>
+        Our Pricing includes a Premium Membership for serious golfers
+        Including additional perks such as free access to the pitching/chipping area,
+        discounted lessons with instructors, and priority booking for tee times.
+        </p>
+      </div>
+      <div className='prices-cnt'>
+        <h1><span className='italic'>Lessons and Practice</span>
+        </h1>
+        <p>
+        Lesson package with instructors.based on instructor's experience 
+        and lesson duration. nominal fee for access to the pitching/chipping area 
+        for non-members, included as a Standard and Premium membership benefit.
+        </p>
+
+
+      </div>
+      <div className='prices-cnt'>
+        <h1><span className='italic'>Standard</span>
+        </h1>
+        <p>
+        competitive annual fees for general access to the golf course
+        and basic facilities such as Tennis, Squash, Braai, Fishing And
+        Swim, All Afiliated which gives you a discount on Golf Days
+        </p>
+
+
+      </div>
+      
+      <div className='prices-cnt'>
+        <h1><span className='italic'>Events and Tournaments</span>
+        </h1>
+        <p>
+        We Host regular tournaments and events, entry fees applicable
+        that cover day costs Event packages for corporate or private events,
+        including catering and use of facilities
+        </p>
+
+
+      </div>
+      </div>
+     
+
+    </section>
+    <footer>
+    <iframe
+  src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d888.6536933979264!2d30.799034969495757!3d-27.010733203717372!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x1eeee49f4b2847ab%3A0x19f0561943a24e6c!2sPiet%20Retief%20Country%20Club!5e0!3m2!1sen!2sza!4v1721138264585!5m2!1sen!2sza&maptype=satellite"
+  width="400"
+  
+  height="300"
+  style={{ border: "2px Solid #4c7766", maxHeight:"100%",  boxSizing:"border-box" }}
+  allowFullScreen=""
+  loading="lazy"
+  referrerPolicy="no-referrer-when-downgrade"
+/>
+
+
+    </footer>
       </section>  
     </>
   );
